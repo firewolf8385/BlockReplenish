985cf04cd7c0af85de18b5e6518dc975c419374e
colby <colbykrawczyk10@gmail.com>
2023-04-24 02:44:46 -0400

    Add ability to generate and load the configuration file

--- src/main/java/net/jadedmc/replenish/Replenish.java
+++ src/main/java/net/jadedmc/replenish/Replenish.java
@@ -3,15 +3,19 @@ package net.jadedmc.replenish;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Replenish extends JavaPlugin {
+    private SettingsManager settingsManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        settingsManager = new SettingsManager(this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    /**
+     * Gets the current settings manager instance.
+     * @return Settings Manager.
+     */
+    public SettingsManager getSettingsManager() {
+        return settingsManager;
     }
 }
